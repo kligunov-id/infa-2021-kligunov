9ef621a19e2af4c1eab2ec7a20f9c09e591dd807
Kligunov <kligunov.id@phystech.edu>
2021-10-01 09:56:19 +0300

    sky

---
+++ lab3/3_draw.py
@@ -0,0 +1,73 @@
+import pygame
+from pygame.draw import *
+
+# Инициализация библиотеки:
+pygame.init()
+
+FPS = 30
+W = 1200
+H = 800
+screen = pygame.display.set_mode((W, H))
+screen.fill((255, 255, 255))
+
+# Цвета
+SKY = (128, 255, 234)
+WHITE = (255, 255, 255)
+GRASS = (34, 204, 0)
+LEAVES = (13, 77, 0)
+BLACK = (0, 0, 0)
+PINK = (255,51,51)
+BROWN = (77, 38, 0)
+GLASS = (0, 153, 153)
+
+def background():
+    polygon(screen, SKY, [
+         (0, 0),
+         (W, 0),
+         (W, H / 2),
+         (0, H / 2)])
+    polygon(screen, GRASS, [
+         (0, H / 2),
+         (W, H / 2),
+         (W, H),
+         (0, W)])
+
+def tree(x, y, scale = 1):
+    pass
+
+def house(x, y, scale = 1):
+    pass
+
+def cloud(x, y, radius = 30, scale = 1):
+    """ Рисует 5 кругов с центром в (x, y) со смещениями DELTA_X и DELTA_Y"""
+    DELTA_X = [-30, -10, 10, 30, -10, 10]
+    DELTA_Y = [15, 15, 15, 15, -15, -15]
+    for dx, dy in zip(DELTA_X, DELTA_Y):
+        circle(screen, WHITE, (x + dx, y + dy), radius * scale)
+        circle(screen, BLACK, (x + dx, y + dy), radius * scale, width = 1) #Окантовка
+
+
+def sun(x, y, radius = 40, scale = 1):
+    circle(screen, GLASS, (x, y), radius * scale)
+
+
+background()
+sun(50, 50)
+cloud(200, 80, scale = 1)
+cloud(600, 110, scale = 0.8)
+cloud(1000, 90, scale = 1.2)
+
+
+# Обновление экрана
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+# Цикл обработки событий
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
