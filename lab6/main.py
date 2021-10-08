94839a62b68f915e6e207cd47477ef97f0adef6e
Kligunov <kligunov.id@phystech.edu>
2021-10-08 10:30:58 +0300

    Demo version finished

--- lab6/main.py
+++ lab6/main.py
@@ -7,7 +7,7 @@ pygame.init()
 FPS = 60
 screen = pygame.display.set_mode((1200, 900))
 
-
+# Colors
 RED  = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -23,9 +23,23 @@ x, y, r, color = 0, 0, 0, RED
 # Ball lifespan
 t = 0
 
+def dist2(p, q):
+    """
+    Returns distance squared between points p and q
+    
+    :param p: Pair of coordinates (x, y) of the first point
+    :param q: Pair of coordinates (x, y) of the second point
+    
+    .. warning:: Distance returned is squared
+    """
+    x1, y1 = p
+    x2, y2 = q
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+
 def new_ball():
     """
-    Creates new ball with randomly chosen position and color
+    Randomly chooses position, radius and color for the ball
     
     :returns: (x_center, y_center, radius, color, lifespan) 
     """
@@ -37,12 +51,17 @@ def new_ball():
     return (x, y, r, color, t)
 
 
-def click():
+def click(ClickEvent):
     """
     Handles mouse clicks events
     Clicked balls should disapper
+
+    :param ClickEvent: mouse event to be handled
     """
-    print('Click')
+    if dist2(ClickEvent.pos, (x, y)) <= r ** 2:
+        global t
+        t = 0
+        print('Hit')
 
 
 # Pygame setup
@@ -59,10 +78,11 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            click()
+            click(event)
     
     # Game engine
     if t <= 0:
+        # Initializing new ball
         x, y, r, color, t = new_ball()
     t -= 1
 
