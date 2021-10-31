7891928d235954436b7181ea2918ce9ad3bd34fb
Kligunov <kligunov.id@phystech.edu>
2021-10-31 19:33:21 +0300

    lab6: Add leaderboard

--- lab6/main.py
+++ lab6/main.py
@@ -34,6 +34,40 @@ class Config:
         Triangle.v = self.data["Triangle"]["v"][difficulty]
         Triangle.v_phi = self.data["Triangle"]["v_phi"][difficulty]
 
+class Leaderboard:
+
+    def __init__(self):
+        self.data = json.load(open("leaderboard.json"))
+        self.data = sorted(self.data, key = lambda a: -int(a[0]))
+
+    
+    def save(self):
+        json.dump(self.data, open("leaderboard.json", "w"), indent = 4)
+
+    def add(self, name, score):
+        while len(name) < 15:
+            name += " "
+
+        score = str(score)
+        while(len(score) < 4):
+            score = " " + score
+        self.data = sorted(self.data + [(score, name)], key = lambda a: -int(a[0]))
+        self.data = self.data[:-1]
+    
+    def render(self):
+        """ Returns PyGame surface with leaderboard """
+        screen = pygame.Surface((WIDTH, HEIGHT * 0.8), pygame.SRCALPHA)
+        font = pygame.font.SysFont(FONT_NAME, 47)
+        text = ["Leaderboard"]
+        for i in range(5):
+            text += [f"{i + 1} {self.data[i][1]} {self.data[i][0]}"]
+        for i in range(6):
+            line = text[i]
+            text_surface = font.render(line, True, BLACK)
+            text_rect = text_surface.get_rect(center = (WIDTH // 2, HEIGHT * (i + 1) * 0.09))
+            screen.blit(text_surface, text_rect)
+        return screen
+
 class GameSession:
 
     N, M = 5, 2
@@ -131,6 +165,7 @@ class Game:
         self.game_over_screen = GameOverScreen()
         self.menu = Menu()
         self.player_name = Game.INITIAL_NAME
+        self.leaderboard = Leaderboard()
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -167,6 +202,7 @@ class Game:
         elif self.state is Game.STATE_FINISHED:
             self.game_session.render(screen, False, Game.FINISHED_GAME_TRANSPARENCY)
             self.game_over_screen.render(screen)
+            screen.blit(self.leaderboard.render(), (0, HEIGHT * 0.18))
         elif self.state is Game.STATE_MENU:
             self.menu.render(screen)
         return screen
@@ -177,7 +213,8 @@ class Game:
         self.state = new_state
         if new_state is Game.STATE_PLAYING:
             self.game_session = GameSession()
-
+        if new_state is Game.STATE_FINISHED:
+            self.leaderboard.add(self.player_name, self.game_session.score)
     def get_score(self):
         return self.game_session.score
 
@@ -190,7 +227,7 @@ class GameOverScreen:
 
     def __init__(self):
         self.font = pygame.font.SysFont(FONT_NAME,  GameOverScreen.FONTSIZE)
-        self.restart_button = Button("Back to menu", (WIDTH / 2, HEIGHT * 0.3)) 
+        self.restart_button = Button("Back to menu", (WIDTH / 2, HEIGHT * 0.85)) 
 
     def render(self, screen: pygame.Surface):
         text_surface_1 = self.font.render(f"Game over, {Game.get_instance().player_name}", True, BLACK)
@@ -331,6 +368,6 @@ def main():
         pygame.display.update()
         screen.fill(WHITE)
     pygame.quit()
-
+    game.leaderboard.save()
 
 main()
